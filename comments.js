a123ae8e440203dc80556cce16838b417fabcf99
JonathonReidMantel <jonathon.reid@mantelgroup.com.au>
2024-08-16 03:53:08 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -2,27 +2,27 @@
 // create a route for comments
 // read the comments from a file
 // return the comments as a response
-const http = require('http');
-const fs = require('fs');
-const comments = require('./data/comments.json');
+// const http = require('http');
+// const fs = require('fs');
+// const comments = require('./data/comments.json');
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/comments' && req.method === 'GET') {
-        fs.readFile('./data/comments.json', (err, data) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'application/json' });
-                res.end(JSON.stringify({ message: 'Internal Server Error' }));
-            } else {
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                res.end(data);
-            }
-        });
-    } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'Not Found' }));
-    }
-});
+// const server = http.createServer((req, res) => {
+//     if (req.url === '/comments' && req.method === 'GET') {
+//         fs.readFile('./data/comments.json', (err, data) => {
+//             if (err) {
+//                 res.writeHead(500, { 'Content-Type': 'application/json' });
+//                 res.end(JSON.stringify({ message: 'Internal Server Error' }));
+//             } else {
+//                 res.writeHead(200, { 'Content-Type': 'application/json' });
+//                 res.end(data);
+//             }
+//         });
+//     } else {
+//         res.writeHead(404, { 'Content-Type': 'application/json' });
+//         res.end(JSON.stringify({ message: 'Not Found' }));
+//     }
+// });
 
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+// server.listen(3000, () => {
+//     console.log('Server is running on http://localhost:3000');
+// });
